63a1a415b0252c8bbf3cbc21cad7ebcafbd60cbb
Jonathan Lee <jonathan.lee.devel@gmail.com>
2020-10-23 21:00:49 +0100

    Added student creation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,8 +9,21 @@ import org.example.model.Student;
 public class App {
 
     public static void main( String[] args ) {
+        Student student1 = createStudent("John Doe", 33, "1987-08-08", 42342348);
+        Student student2 = createStudent("Jane Doe", 23, "1997-08-08", 93837562);
+        Student student3 = createStudent("Ryan Crest", 21, "1999-08-08", 39485729);
+        Student student4 = createStudent("Hannah Albert", 22, "1998-08-08", 30293847);
+        Student student5 = createStudent("Sarah Ford", 23, "1997-08-08", 12293847);
+    }
+
+    public static Student createStudent(String name, int age, String dob, int id) {
         Student student = new Student();
-        System.out.println(student);
+        student.setName(name);
+        student.setAge(age);
+        student.setDob(dob);
+        student.setId(id);
+
+        return student;
     }
 
 }
