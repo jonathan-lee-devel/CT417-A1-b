18d8cd7cb383140ff560c1f867e4eb472e60e177
Jonathan Lee <jonathan.lee.devel@gmail.com>
2020-10-23 20:52:17 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.model.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        Student student = new Student();
+        System.out.println(student);
+    }
+
+}
